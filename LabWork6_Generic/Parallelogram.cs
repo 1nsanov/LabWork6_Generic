4ceea29e105264d899bc7d893c3a6d8aa37ed269
1nsanov <victorbezunov@gmail.com>
2022-02-10 21:12:14 +0200

    Добавьте файлы проекта.

---
+++ LabWork6_Generic/Parallelogram.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabWork6_Generic
+{
+    public class Parallelogram<T>
+        where T : struct
+    {
+        public T SideA { get; set; }
+        public T SideB { get; set; }
+        public T Angle { get; set; }
+
+        public Parallelogram(T sideA, T sideB, T angle)
+        {
+            if (Convert.ToDouble((dynamic)sideA) < 0 || Convert.ToDouble((dynamic)sideB) < 0)
+            {
+                throw new Exception("Сторона не может быть меньше нуля");
+            }
+            if (Convert.ToDouble((dynamic)angle) < 0)
+            {
+                throw new Exception("Угол не может быть меньше нуля");
+            }
+            SideA = sideA;
+            SideB = sideB;
+            Angle = angle;
+        }
+
+        public double CalcSquare()
+        {
+            var square = (dynamic)SideA * (dynamic)SideB;
+            return Math.Round(Convert.ToDouble(square), 2);
+        }
+
+        public override string ToString()
+        {
+            return $"A: {SideA} | B:{SideB} | Угол: {Angle} | {CalcSquare()}";
+        }
+    }
+}
---
+++ LabWork6_Generic/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabWork6_Generic
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var parallelogramInt = new Parallelogram<int>(5, 8, 60);
+            var parallelogramFloat = new Parallelogram<float>(6.3f, 3.4f, -60f);
+            var parallelogramDouble = new Parallelogram<double>(8.5, 5.7, 60);
+
+            Console.WriteLine(parallelogramInt.CalcSquare());
+            Console.WriteLine(parallelogramFloat.CalcSquare());
+            Console.WriteLine(parallelogramDouble.CalcSquare());
+
+            Console.ReadLine();
+        }
+    }
+}
